866bccff112f2b5dfc746ffb4ea76651811d8db5
itskegnh <itskegnh@gmail.com>
2023-04-28 16:03:15 +1000

    day 1 changes

--- __main__.py
+++ __main__.py
@@ -1,14 +1,229 @@
-import disnake, json, pickle
+import disnake, json, pickle, random, asyncio
 from disnake.ext import commands
-import tank
+from disnake.interactions import MessageInteraction
+from tank import *
 from io import BytesIO
 
 bot = commands.InteractionBot(test_guilds=[791818283867045941])
 
+
 class TankCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.tankgrid = tank.TankGrid(15, 10, 256)
+        self.tankgrid = TankGrid(15, 10, 256)
+        self.livepreview_msg = None
+    
+    async def _image_to_bytes(self, image):
+        if image is None:
+            return None
+        _img = BytesIO()
+        image.save(_img, 'png')
+        _img.seek(0)
+
+        return _img
+    
+    async def _updatemap(self):
+        if self.livepreview_msg is None:
+            return
+        
+        await self.livepreview_msg.edit(content='<a:loading:1086100481849430036> Updating Live Preview...')
+        
+        image = await self.tankgrid.create_image()
+        image = await self._image_to_bytes(image)
+
+        await self.livepreview_msg.edit(content='', embed=disnake.Embed.from_dict({
+            'title': 'Tank Tactics - Live Map',
+            'image': {
+                'url': 'attachment://map.png',
+            },
+            'color': 0x2b2d31
+        }),file=disnake.File(image, filename='map.png'))
+
+    @commands.slash_command(
+        name='livepreview', 
+        description='Start a live preview of the tank grid.',
+    )
+    @commands.default_member_permissions(manage_guild=True)
+    async def _livepreview(self, inter : disnake.ApplicationCommandInteraction):
+        await inter.response.defer(with_message=True, ephemeral=True)
+
+        msg = await inter.channel.send('<a:loading:1086100481849430036> Starting Live Preview...')
+        self.livepreview_msg = msg
+
+        await inter.followup.send('Live Preview Started!', ephemeral=True)
+
+        await self._updatemap()
+
+    @commands.slash_command(
+        name='tank',
+        description='Create a tank.',
+    )
+    async def _tank(self, inter : disnake.ApplicationCommandInteraction):
+        await inter.response.defer(with_message=True, ephemeral=True)
+        
+        for pos, tank in self.tankgrid.tanks.items():
+            if tank.user.id == inter.author.id: break
+        else:
+            tank = Tank(self.tankgrid, inter.author)
+            await tank._fetch_avatar()
+            while True:
+                try:
+                    await tank._place(random.randint(0, self.tankgrid.width-1), random.randint(0, self.tankgrid.height-1))
+                    break
+                except IndexError: ...
+            asyncio.create_task(self._updatemap())
+        
+        async def get_map():
+            image = await self.tankgrid.create_tank_image(tank)
+            image = await self._image_to_bytes(image)
+            file = disnake.File(image, filename='map.png')
+            return file
+
+        async def movement_view():
+            view = disnake.ui.View(timeout=None)
+
+            empty_button = lambda row=0: view.add_item(disnake.ui.Button(style=disnake.ButtonStyle.gray, emoji='<:nothing:1101337783328575569>', disabled=True, row=row))
+            
+            class MoveButton(disnake.ui.Button):
+                def __init__(self, tank, direction, *args, **kwargs):
+                    super().__init__(*args, **kwargs)
+                    self.tank = tank
+                    self.style = disnake.ButtonStyle.gray
+                    self.direction = direction
+
+                    if not self.tank.can_move(self.direction):
+                        self.disabled = True
+
+                async def callback(button_self, interaction : disnake.MessageInteraction):
+                    # await interaction.response.defer()
+                    await button_self.tank.move(button_self.direction)
+                    # await self._updatemap()
+                    asyncio.create_task(self._updatemap())
+                    await interaction.response.edit_message(view=await movement_view(), file=await get_map())
+            
+            class ShootButton(disnake.ui.Button):
+                def __init__(self, tank, *args, **kwargs):
+                    super().__init__(*args, **kwargs)
+                    self.tank = tank
+                    self.row = 1
+                    self.emoji = '<:bang:931340204051152926>'
+                    self.style = disnake.ButtonStyle.gray
+
+                    # if self.tank.can_shoot():
+                    #     self.disabled = False
+
+                async def callback(self, interaction : disnake.MessageInteraction):
+                    await interaction.response.defer()
+                    # self.tank.shoot()
+                    # await self._updatemap()
+            
+            class UpgradeRange(disnake.ui.Button):
+                def __init__(self, tank, *args, **kwargs):
+                    super().__init__(*args, **kwargs)
+                    self.tank = tank
+                    self.row = 0
+                    self.emoji = '<:earth:931340204332183602>'
+                    self.style = disnake.ButtonStyle.gray
+
+                    # if self.tank.can_shoot():
+                    #     self.disabled = False
+                
+                async def callback(self, interaction: MessageInteraction):
+                    self.tank.range += 1
+                    await interaction.response.edit_message(view=await movement_view(), file=await get_map())
+
+            class PurchaseHeart(disnake.ui.Button):
+                def __init__(self, tank, *args, **kwargs):
+                    super().__init__(*args, **kwargs)
+                    self.tank = tank
+                    self.row = 1
+                    self.emoji = '<:heart:931356446887669760>'
+                    self.style = disnake.ButtonStyle.gray
+
+                    # if self.tank.can_shoot():
+                    #     self.disabled = False
+            
+            class GiftItem(disnake.ui.Button):
+                def __init__(self, tank, *args, **kwargs):
+                    super().__init__(*args, **kwargs)
+                    self.tank = tank
+                    self.row = 2
+                    self.emoji = '<:crate_epic:883811932157648917>'
+                    self.style = disnake.ButtonStyle.gray
+
+                    # if self.tank.can_shoot():
+                    #     self.disabled = False
+
+            
+            empty_button()
+            view.add_item(MoveButton(tank, 3, emoji='<:arrow_up:931340203992449064>', row=0))
+            empty_button()
+            empty_button()
+            view.add_item(UpgradeRange(tank))
+
+            view.add_item(MoveButton(tank, 2, emoji='<:arrow_left:931340204051169312>', row=1))
+            view.add_item(ShootButton(tank))
+            view.add_item(MoveButton(tank, 0, emoji='<:arrow_right:931340204021792868>', row=1))
+            empty_button(1)
+            view.add_item(PurchaseHeart(tank))
+
+            empty_button(2)
+            view.add_item(MoveButton(tank, 1, emoji='<:arrow_down:931340204025983066>', row=2))
+            empty_button(2)
+            empty_button(2)
+            view.add_item(GiftItem(tank))
+
+
+            return view
+            
+
+        
+
+        # create the embed
+        embed = disnake.Embed.from_dict({
+            'author': {
+                'name': f'{tank.user.display_name}\'s Tank',
+                'icon_url': 'attachment://avatar.png',
+            },
+            'image': {
+                'url': 'attachment://map.png',
+            },
+            'description': '<:heart:931356446887669760><:heart:931356446887669760><:heart:931356446887669760>\n<:dollar:931340204495745114> `1`',
+            'color': 0x2b2d31,
+        })
+
+        view = await movement_view()
+
+
+
+        gift = disnake.ui.Button(style=disnake.ButtonStyle.gray, emoji='<:crate_epic:883811932157648917>')
+        buy_heart = disnake.ui.Button(style=disnake.ButtonStyle.gray, emoji='<:heart:931356446887669760>')
+        upgrade_range = disnake.ui.Button(style=disnake.ButtonStyle.gray, emoji='<a:bang:931340204051152926>')
+
+        await inter.followup.send(embed=embed, files=[disnake.File(await self._image_to_bytes(tank.image), filename='avatar.png'), await get_map()], view=view)
+
+    @commands.slash_command(
+        name='suicide',
+        description='Commit suicide.'
+    )
+    async def _suicide(self, inter : disnake.ApplicationCommandInteraction):
+
+        await inter.response.defer(with_message=True, ephemeral=True)
+
+        for pos, tank in self.tankgrid.tanks.items():
+            if tank.user.id == inter.author.id:
+                break
+        else:
+            return await inter.followup.send('You do not have a tank. Use /tank.')
+        
+        tank.kill()
+
+        asyncio.create_task(self._updatemap())
+
+
+
+
+
 
 bot.add_cog(TankCog(bot))
 
--- tank.py
+++ tank.py
@@ -1,4 +1,4 @@
-import httpx
+import httpx, asyncio
 from io import BytesIO
 from PIL import Image, ImageDraw
 
@@ -11,7 +11,7 @@ class TankGrid:
 
         self.tanks = {}
     
-    def _create_grid(self, width, height):
+    async def _create_grid(self, width, height):
         image = Image.new('RGB', (width*self.cellsize, height*self.cellsize), (255, 255, 255))
         draw = ImageDraw.Draw(image)
 
@@ -21,14 +21,45 @@ class TankGrid:
         
         return image
     
-    def create_image(self):
-        image = self._create_grid(self.width, self.height)
+    async def create_image(self):
+        image = await self._create_grid(self.width, self.height)
 
         for tank in self.tanks:
             x, y = tank
-            image.paste(self.tanks[tank], (x*self.cellsize+self.outline_width, y*self.cellsize+self.outline_width))
+            image.paste(self.tanks[tank].image, (x*self.cellsize+self.outline_width, y*self.cellsize+self.outline_width))
         
         return image
+    
+    async def create_tank_image(self, tank):
+        for pos, _tank in self.tanks.items():
+            if _tank.user.id == tank.user.id: break
+        else:
+            raise ValueError('Tank not found')
+        
+        x, y = pos
+        
+        image = await self._create_grid(tank.range*2+1, tank.range*2+1)
+        
+        for _x in range(x-tank.range, x+tank.range+1):
+            for _y in range(y-tank.range, y+tank.range+1):
+                if (_x, _y) in self.tanks:
+                    image.paste(self.tanks[(_x, _y)].image, ((_x-x+tank.range)*self.cellsize+self.outline_width, (_y-y+tank.range)*self.cellsize+self.outline_width))
+                
+        # draw range
+        draw = ImageDraw.Draw(image)
+        draw.rectangle((self.cellsize, self.cellsize, (tank.range*2)*self.cellsize, (tank.range*2)*self.cellsize), outline=(255, 0, 0), width=self.outline_width)
+
+        # draw black over cells not in bounds
+        for virtual_x in range(tank.range*2+1):
+            for virtual_y in range(tank.range*2+1):
+                # convert local (virtual) coordinates to global coordinates
+                real_x, real_y = virtual_x + x-tank.range, virtual_y + y-tank.range
+                if real_x < 0 or real_y < 0 or real_x >= self.width or real_y >= self.height:
+                    draw.rectangle((virtual_x*self.cellsize, virtual_y*self.cellsize, (virtual_x+1)*self.cellsize, (virtual_y+1)*self.cellsize), fill=(0, 0, 0))
+
+        return image
+
+
 
 class Tank:
     def __init__(self, tank_grid, user):
@@ -36,20 +67,25 @@ class Tank:
         self.tank_grid = tank_grid
         
         # get image
-        response = httpx.get(user.avatar.url)
-        self.image = Image.open(BytesIO(response.content))
-        self.image = self.image.resize(
-            (tank_grid.cellsize-tank_grid.outline_width*2+1, 
-             tank_grid.cellsize-tank_grid.outline_width*2+1)
-        )
-        self.image = self.image.convert('RGB')
+        self.image = None
 
         self.x, self.y = None, None
 
         self.action_points = 0
         self.is_dead = False
+        self.range = 2
+    
+    async def _fetch_avatar(self):
+        async with httpx.AsyncClient() as client:
+            response = await client.get(self.user.avatar.url)
+            self.image = Image.open(BytesIO(response.content))
+            self.image = self.image.resize(
+                (self.tank_grid.cellsize-self.tank_grid.outline_width*2+1, 
+                self.tank_grid.cellsize-self.tank_grid.outline_width*2+1)
+            )
+            self.image = self.image.convert('RGB')
 
-    def _place(self, x, y):
+    async def _place(self, x, y):
         
         # check if there is a tank already there
         if (x, y) in self.tank_grid.tanks:
@@ -60,22 +96,48 @@ class Tank:
             del self.tank_grid.tanks[(self.x, self.y)]
         
         # place tank
-        self.tank_grid.tanks[(x, y)] = self.image
+        self.tank_grid.tanks[(x, y)] = self
+
+        self.x, self.y = x, y
         
 
-    def move(self, direction):
+    async def move(self, direction):
         try:
             if direction == 0:
-                self._place(self.x+1, self.y)
+                await self._place(self.x+1, self.y)
             elif direction == 1:
-                self._place(self.x, self.y+1)
+                await self._place(self.x, self.y+1)
             elif direction == 2:
-                self._place(self.x-1, self.y)
+                await self._place(self.x-1, self.y)
             elif direction == 3:
-                self._place(self.x, self.y-1)
+                await self._place(self.x, self.y-1)
         except IndexError: ...
     
-    def kill(self):
+    def can_move(self, direction):
+        # check bounds and players
+        if direction == 0:
+            if self.x+1 >= self.tank_grid.width:
+                return False
+            if (self.x+1, self.y) in self.tank_grid.tanks:
+                return False
+        elif direction == 1:
+            if self.y+1 >= self.tank_grid.height:
+                return False
+            if (self.x, self.y+1) in self.tank_grid.tanks:
+                return False
+        elif direction == 2:
+            if self.x-1 < 0:
+                return False
+            if (self.x-1, self.y) in self.tank_grid.tanks:
+                return False
+        elif direction == 3:
+            if self.y-1 < 0:
+                return False
+            if (self.x, self.y-1) in self.tank_grid.tanks:
+                return False
+        return True
+    
+    async def kill(self):
         self.is_dead = True
         self.image = self.image.convert('L')
 
